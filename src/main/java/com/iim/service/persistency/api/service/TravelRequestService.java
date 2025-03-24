9dee945157cc43fd08151a919b8d0336dd7be97f
casarin <nsgbanks@ciencias.unam.mx>
2025-03-23 23:29:32 -0600

    chore: add body for service

--- src/main/java/com/iim/service/persistency/api/service/TravelRequestService.java
+++ src/main/java/com/iim/service/persistency/api/service/TravelRequestService.java
@@ -1,4 +1,9 @@
 package com.iim.service.persistency.api.service;
 
+import com.iim.service.persistency.api.dto.CreateTravelRequestDTO;
+import org.springframework.stereotype.Service;
+
+@Service
 public interface TravelRequestService {
+    void createTravelRequest(CreateTravelRequestDTO input);
 }
---
+++ src/main/java/com/iim/service/persistency/api/service/TravelRequestServiceImpl.java
@@ -0,0 +1,12 @@
+package com.iim.service.persistency.api.service;
+
+import com.iim.service.persistency.api.dto.CreateTravelRequestDTO;
+
+public class TravelRequestServiceImpl implements TravelRequestService{
+
+
+    @Override
+    public void createTravelRequest(CreateTravelRequestDTO input) {
+
+    }
+}
