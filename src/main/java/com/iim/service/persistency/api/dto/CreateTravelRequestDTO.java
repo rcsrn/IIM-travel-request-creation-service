1c477b4f07e99846af112b0d2e6d3db8cb359153
casarin <nsgbanks@ciencias.unam.mx>
2025-03-23 23:49:16 -0600

    feat: add validation for CreateTravelRequestDTO

--- src/main/java/com/iim/service/persistency/api/dto/CreateTravelRequestDTO.java
+++ src/main/java/com/iim/service/persistency/api/dto/CreateTravelRequestDTO.java
@@ -1,8 +1,11 @@
 package com.iim.service.persistency.api.dto;
 
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 import java.util.List;
+
+import static com.iim.service.persistency.api.constant.Constants.MAX_CONCEPT_LIST_SIZE_MESSAGE;
 
 @Data
 public class CreateTravelRequestDTO {
@@ -10,5 +13,6 @@ public class CreateTravelRequestDTO {
 
     private TravelRequestDTO travelRequestDTO;
 
+    @Size(max = 4, message = MAX_CONCEPT_LIST_SIZE_MESSAGE)
     private List<ConceptDTO> conceptDTO;
 }
