97d3d06ffe9759701f6bc83cac88ac9bd7562d86
casarin <nsgbanks@ciencias.unam.mx>
2025-03-24 00:02:02 -0600

    feat: add error handling in TravelRequestController

--- src/main/java/com/iim/service/persistency/api/controller/TravelRequestController.java
+++ src/main/java/com/iim/service/persistency/api/controller/TravelRequestController.java
@@ -1,4 +1,33 @@
 package com.iim.service.persistency.api.controller;
 
+import com.iim.service.persistency.api.dto.CreateTravelRequestDTO;
+import com.iim.service.persistency.api.entity.TravelRequest;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static com.iim.service.persistency.api.constant.Paths.baseUrl;
+
+@RestController
+@RequestMapping(baseUrl)
 public class TravelRequestController {
+
+    public ResponseEntity<?> createTravelRequest(@RequestBody @Valid CreateTravelRequestDTO createTravelRequestDTO, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            List<String> errorMessages = bindingResult.getAllErrors().stream().
+                    map(ObjectError::getDefaultMessage).
+                    toList();
+            return ResponseEntity.badRequest().body(errorMessages);
+        }
+
+        return null;
+    }
 }
+
