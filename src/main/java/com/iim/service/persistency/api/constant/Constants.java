31c7c5db58461fd11f95daba29cff68ab0d5d1e9
casarin <nsgbanks@ciencias.unam.mx>
2025-03-23 23:48:43 -0600

    chore: add new constants

--- src/main/java/com/iim/service/persistency/api/constant/Constants.java
+++ src/main/java/com/iim/service/persistency/api/constant/Constants.java
@@ -3,13 +3,22 @@ package com.iim.service.persistency.api.constant;
 public class Constants {
 
     public final static String NO_FIRST_NAME_MESSAGE = "First name is required";
+
     public final static String NO_FIRST_LAST_NAME_MESSAGE = "First last name is required";
+
     public final static String NO_SECOND_LAST_NAME_MESSAGE = "Second last name is required";
+
     public final static String NO_START_REQUEST_DATE_MESSAGE = "Start travel request date is required";
+
     public final static String NO_END_REQUEST_DATE_MESSAGE = "End travel request date is required";
+
     public final static String NO_START_ABSENCE_DATE_MESSAGE = "Start date of absence is required";
+
     public final static String NO_END_ABSENCE_DATE_MESSAGE = "End date of absence is required";
+
     public final static String NO_REQUEST_TYPE_MESSAGE = "Request type is required";
+
     public final static String NO_KINSHIP_MESSAGE = "Kinship of insurance is required";
 
+    public final static String MAX_CONCEPT_LIST_SIZE_MESSAGE = "Concept list must contain max four elements";
 }
--- src/main/java/com/iim/service/persistency/api/constant/Paths.java
+++ src/main/java/com/iim/service/persistency/api/constant/Paths.java
@@ -1,4 +1,6 @@
 package com.iim.service.persistency.api.constant;
 
 public class Paths {
+
+    public static final String baseUrl = "/iim/v1/travelRequest";
 }
