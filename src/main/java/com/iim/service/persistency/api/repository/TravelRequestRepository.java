ce46ba48fd73664df3f9e6acedc2d239ea64d141
casarin <nsgbanks@ciencias.unam.mx>
2025-03-25 21:51:06 -0600

    feat: add Repository annotation to repositories

--- src/main/java/com/iim/service/persistency/api/repository/TravelRequestRepository.java
+++ src/main/java/com/iim/service/persistency/api/repository/TravelRequestRepository.java
@@ -3,6 +3,8 @@ package com.iim.service.persistency.api.repository;
 
 import com.iim.service.persistency.api.entity.TravelRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface TravelRequestRepository extends JpaRepository<TravelRequest, Long> {
 }
