5204f4ea361e8310a322cc3529d06e5daddec9ec
casarin <nsgbanks@ciencias.unam.mx>
2025-03-26 20:06:30 -0600

    feat: TestDataFactory created to create data for testing

--- src/main/java/com/iim/service/persistency/api/dto/ConceptDTO.java
+++ src/main/java/com/iim/service/persistency/api/dto/ConceptDTO.java
@@ -1,10 +1,14 @@
 package com.iim.service.persistency.api.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class ConceptDTO {
 
     private Long projectNumber;
--- src/test/java/com/iim/service/persistency/api/service/TravelRequestServiceTest.java
+++ src/test/java/com/iim/service/persistency/api/service/TravelRequestServiceTest.java
@@ -1,9 +1,8 @@
 package com.iim.service.persistency.api.service;
 
+import com.iim.service.persistency.api.dto.CreateTravelRequestDTO;
 import com.iim.service.persistency.api.entity.Concept;
 import com.iim.service.persistency.api.entity.Subtotal;
-import com.iim.service.persistency.api.entity.TravelRequest;
-import com.iim.service.persistency.api.entity.User;
 import com.iim.service.persistency.api.repository.ConceptRepository;
 import com.iim.service.persistency.api.repository.SubtotalRepository;
 import com.iim.service.persistency.api.repository.TravelRequestRepository;
@@ -15,9 +14,7 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import java.math.BigDecimal;
-import java.time.LocalDateTime;
-import java.util.Date;
+import static com.iim.service.persistency.util.TestDataFactory.*;
 
 @ExtendWith(MockitoExtension.class)
 public class TravelRequestServiceTest {
@@ -39,52 +36,12 @@ public class TravelRequestServiceTest {
 
     @Test
     void createTravelRequest_whenThereAreConcepts_ShouldSaveEntities() {
+        Mockito.when(userRepository.save(Mockito.any())).thenReturn(buildUser());
+        Mockito.when(travelRequestRepository.save(Mockito.any())).thenReturn(buildTravelRequest());
+        Mockito.when(subtotalRepository.save(Mockito.any())).thenReturn(new Subtotal());
+        Mockito.when(conceptRepository.save(Mockito.any())).thenReturn(new Concept());
 
+        travelRequestService.createTravelRequest(new CreateTravelRequestDTO());
     }
 
-    private User generateUser() {
-        User user = new User();
-        user.setId(1L);
-        user.setFirstName("Juan");
-        user.setSecondName("Carlos");
-        user.setFirstLastName("Lopez");
-        user.setSecondLastName("Perez");
-        user.setRole("Admin");
-        return user;
-    }
-
-    private TravelRequest generateTravelRequest() {
-        LocalDateTime nowDate = LocalDateTime.now();
-
-        TravelRequest travelRequest = new TravelRequest();
-        travelRequest.setId(1L);
-        travelRequest.setUser(generateUser());
-        travelRequest.setPlace("Guadalajara MX");
-        travelRequest.setMotive("concurso de programacion");
-        travelRequest.setActivity("calificacion de programas");
-        travelRequest.setJob("juez");
-        travelRequest.setModality("Presencial");
-        travelRequest.setSerie("SV001");
-        travelRequest.setStartDateRequest(nowDate);
-        travelRequest.setEndDateRequest(nowDate.plusDays(20));
-        travelRequest.setStartDateAbsence(nowDate.plusDays(1));
-        travelRequest.setEndDateAbsence(nowDate.plusDays(19));
-        travelRequest.setInsuranceFirstName("Pedro");
-        travelRequest.setInsuranceFirstLastName("Hernandez");
-        travelRequest.setInsuranceSecondLastName("Rodríguez");
-        travelRequest.setInsuranceKinship("Padre");
-        travelRequest.setTotalPrice(new BigDecimal("1601.61"));
-        return travelRequest;
-    }
-
-
-    private Subtotal generateSubtotal() {
-        Subtotal subtotal = new Subtotal();
-        subtotal.setId(1L);
-        subtotal.setTravelRequest(generateTravelRequest());
-        subtotal.setInscription(new BigDecimal("153.15"));
-        subtotal.setFare(new BigDecimal("1321.10"));
-        subtotal.setPerDiem(new BigDecimal("1684.99"));
-        return subtotal;
-    }
 }
