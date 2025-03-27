bb904df1641cfc7a27e4893593a550cb23c136a3
casarin <nsgbanks@ciencias.unam.mx>
2025-03-26 20:45:38 -0600

    refactor: add podam dependency and change logic of methods in TestDataFactory

--- src/test/java/com/iim/service/persistency/util/TestDataFactory.java
+++ src/test/java/com/iim/service/persistency/util/TestDataFactory.java
@@ -7,84 +7,40 @@ import com.iim.service.persistency.api.dto.UserDTO;
 import com.iim.service.persistency.api.entity.Subtotal;
 import com.iim.service.persistency.api.entity.TravelRequest;
 import com.iim.service.persistency.api.entity.User;
+import uk.co.jemos.podam.api.PodamFactory;
+import uk.co.jemos.podam.api.PodamFactoryImpl;
+
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class TestDataFactory {
     private TestDataFactory() {}
 
+    private final static PodamFactory podamFactory = new PodamFactoryImpl();
+
     public static User buildUser() {
-        User user = new User();
-        user.setId(1L);
-        user.setFirstName("Juan");
-        user.setSecondName("Carlos");
-        user.setFirstLastName("Lopez");
-        user.setSecondLastName("Perez");
-        user.setRole("Admin");
-        return user;
+        return podamFactory.manufacturePojo(User.class);
     }
 
     public static TravelRequest buildTravelRequest() {
-        LocalDateTime nowDate = LocalDateTime.now();
-
-        TravelRequest travelRequest = new TravelRequest();
-        travelRequest.setId(1L);
-        travelRequest.setUser(buildUser());
-        travelRequest.setPlace("Guadalajara MX");
-        travelRequest.setMotive("concurso de programacion");
-        travelRequest.setActivity("calificacion de programas");
-        travelRequest.setJob("juez");
-        travelRequest.setModality("Presencial");
+        TravelRequest travelRequest =  podamFactory.manufacturePojo(TravelRequest.class);
         travelRequest.setSerie("SV001");
-        travelRequest.setStartDateRequest(nowDate);
-        travelRequest.setEndDateRequest(nowDate.plusDays(20));
-        travelRequest.setStartDateAbsence(nowDate.plusDays(1));
-        travelRequest.setEndDateAbsence(nowDate.plusDays(19));
-        travelRequest.setInsuranceFirstName("Pedro");
-        travelRequest.setInsuranceFirstLastName("Hernandez");
-        travelRequest.setInsuranceSecondLastName("Rodríguez");
-        travelRequest.setInsuranceKinship("Padre");
-        travelRequest.setTotalPrice(new BigDecimal("1601.61"));
         return travelRequest;
     }
 
-    private CreateTravelRequestDTO buildCreateTravelRequestDTO() {
-        CreateTravelRequestDTO createTravelRequestDTO = new CreateTravelRequestDTO();
-
-        createTravelRequestDTO.setTravelRequestDTO(buoldTravelRequestDTO());
-
-        createTravelRequestDTO.setUserDTO(buildUserDTO());
-
-        createTravelRequestDTO.setConcepts(buildConceptDTOList());
-
+    public static CreateTravelRequestDTO buildCreateTravelRequestDTO() {
+        CreateTravelRequestDTO createTravelRequestDTO = podamFactory.manufacturePojo(CreateTravelRequestDTO.class);
+        createTravelRequestDTO.setConcepts(Collections.singletonList(new ConceptDTO(1L, "type", new BigDecimal("10.21"), new BigDecimal("20.01"), new BigDecimal("302.1231230"))));
         return createTravelRequestDTO;
     }
 
-    private List<ConceptDTO> buildConceptDTOList() {
-        List<ConceptDTO> conceptDTOList = new ArrayList<>();
-        conceptDTOList.add(new ConceptDTO(1L, "CONACYT", new BigDecimal("123.125156"), new BigDecimal("0.0"), new BigDecimal("0")));
-        conceptDTOList.add(new ConceptDTO(1L, "CONACYT", new BigDecimal("123.125156"), new BigDecimal("156.48"), new BigDecimal("0")));
-        conceptDTOList.add(new ConceptDTO(1L, "CONACYT", new BigDecimal("123.12235156"), new BigDecimal("156.4"), new BigDecimal("0")));
-        return conceptDTOList;
+    public static CreateTravelRequestDTO buildCreateTravelRequestDTOWithNoConcepts() {
+        CreateTravelRequestDTO createTravelRequestDTO = podamFactory.manufacturePojo(CreateTravelRequestDTO.class);
+        createTravelRequestDTO.setConcepts(null);
+        return createTravelRequestDTO;
     }
 
-    private UserDTO buildUserDTO() {
-        return null;
-    }
-
-    private TravelRequestDTO buoldTravelRequestDTO() {
-        return null;
-    }
-
-    private Subtotal buildSubtotal() {
-        Subtotal subtotal = new Subtotal();
-        subtotal.setId(1L);
-        subtotal.setTravelRequest(buildTravelRequest());
-        subtotal.setInscription(new BigDecimal("153.15"));
-        subtotal.setFare(new BigDecimal("1321.10"));
-        subtotal.setPerDiem(new BigDecimal("1684.99"));
-        return subtotal;
-    }
 }
