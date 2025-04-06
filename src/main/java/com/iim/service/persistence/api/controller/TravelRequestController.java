ab013d04b40a6d347d6ed57773db89b68eed5002
casarin <nsgbanks@ciencias.unam.mx>
2025-04-05 22:25:29 -0600

    refactor: remove unnecessary import

--- src/main/java/com/iim/service/persistence/api/controller/TravelRequestController.java
+++ src/main/java/com/iim/service/persistence/api/controller/TravelRequestController.java
@@ -9,7 +9,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.BindingResult;
 import org.springframework.validation.Errors;
 import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.PostMapping;
